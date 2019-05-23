cc228adce8336b06a54bb3c983bc9b140ec2f544
edelmira.morataya <edelmorataya@gmail.com>
2019-05-22 19:30:09 -0600

    update Libraries

--- app/app.js
+++ app/app.js
@@ -1,7 +1,24 @@
+angular.module ('angularapp', ["ui.router"] );
 
+var config = function ($stateProvider, $locationProvider, $urlRouterProvider){
+  let states = [{
+    name: 'app',
+    options: {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'app/app.html',
+        controller: 'AppController',
+        controllerAs: 'vm'
+    }
+  },{
+      name: 'app.carrousel',
+      options: {
+          url: '/carrousel',
+          templateUrl: 'app/js/controllers/carrousel/carrousel.html',
+          controller: 'CarrouselController',
+          controllerAs: 'vm'
+      }
 
-angular.module ('carrousel', ['ui-bootstrap'] ) {
-  var slides = $scope.slides = [];
+  }];
 
-
-}
+};
